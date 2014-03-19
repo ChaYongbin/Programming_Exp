ba0b447909662c4924ef029419e4df194ab84efb
teddy418 <teddy418@nhnnext.org>
2014-03-19 14:19:49 +0900

    mainapp3

---
+++ chapter3/mainapp3.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Floatingpoint
+{
+	class MainApp
+	{
+		static void Main(string[] args)
+		{
+			float a = 3.14159265358979323846f;
+			double b = 3.14159265368979323846;
+
+			Console.WriteLine(a);
+			Console.WriteLine(b);
+
+		}
+	}
+}
+
