34d359c2fc1125860f2c70418834a66a87d60667
teddy418 <teddy418@nhnnext.org>
2014-03-19 10:56:24 +0900

    mkcs mainapp1

---
+++ chapter3/mainapp1.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace SignedUnsinged
+{
+	class mainapp
+	{
+		static void Main(string[] args)
+		{
+				byte a = 255;
+				sbyte b = (sbyte)a;
+
+				Console.WriteLine(a);
+				Console.WriteLine(b);
+		}
+	}
+}
+
+		
