a0ad445c5ea8f4dd2b8a9c432b1fad83c0511775
teddy418 <teddy418@nhnnext.org>
2014-03-28 19:42:24 +0900

    Mlab5 upload

---
+++ mypex/chapter5/mlab5.cs
@@ -0,0 +1,18 @@
+using System;
+// 10이하의 숫자를 나타내는 코드
+namespace Mlab5
+{
+	class MainApp
+	{
+		static void Main(string[] args)
+		{
+			int n = 0;
+
+			while (n < 10)
+			{
+				Console.WriteLine("n={0}",n++);
+			}
+		}
+	}
+}
+
