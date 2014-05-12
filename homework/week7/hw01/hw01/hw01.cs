7d28adc380d2e93a611cf96ac25004f8312f0958
Chayongbin <teddy418@nhnnext.org>
2014-05-12 21:34:39 +0900

    week7

---
+++ homework/week7/hw01/hw01/hw01.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw01
+{
+    class hw01
+    {
+        static void Swap(List<int> n, int pos1, int pos2) //Swap 구현
+        {
+            int t1 = n.ElementAt(pos1);
+            int t2 = n.ElementAt(pos2);
+
+            n.RemoveAt(pos1);
+            n.Insert(pos1, t2);
+            n.RemoveAt(pos2);
+            n.Insert(pos2, t1);
+        }
+        
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            List <int> ll = new List<int>();
+            
+            for (int i = 0; i < 11 ; i++)
+            {
+                ll.Add(i);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Swap(ll, r.Next(0,10-i), 10-i);
+            }
+
+            foreach (int i in ll)
+                Console.Write(i + " ");
+        }
+    }
+}
---
+++ homework/week7/hw02/hw02/hw02.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw02
+{
+    class hw02
+    {
+        static void Swap(List<int> n, int pos1, int pos2) //Swap 구현
+        {
+            int t1 = n.ElementAt(pos1);
+            int t2 = n.ElementAt(pos2);
+
+            n.RemoveAt(pos1);
+            n.Insert(pos1, t2);
+            n.RemoveAt(pos2);
+            n.Insert(pos2, t1);
+        }
+
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            List<int> ll = new List<int>();
+
+            for (int i = 0; i < 46; i++)
+            {
+                ll.Add(i);
+            }
+
+            for (int i = 0; i < 45; i++)
+            {
+                Swap(ll, r.Next(0, 45 - i), 45 - i);
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("| {0:00} ", ll.ElementAt(i));
+            }
+        }
+    }
+}
